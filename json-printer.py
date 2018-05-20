5595c482fc9a6afa048ee3030903a4881936a261
pulnara <magnes.agnieszka@gmail.com>
2018-05-20 21:17:38 +0200

    Add basic JSON parsing

--- json-printer.py
+++ json-printer.py
@@ -1,24 +1,53 @@
 import sys
 import json
 from argparse import ArgumentParser
-from graphics import *
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from os.path import exists
+
 
 def check_args(args):
-    print("Input file: " + args.input_file)
+    print('Input file: ' + args.input_file)
     input_file_splitted = args.input_file.split('.')
     if len(input_file_splitted) != 2 or input_file_splitted[1] != 'json':
-        raise ValueError("Invalid inputfile name.")
+        raise ValueError('Invalid input file name.')
+    if not exists(args.input_file):
+        raise ValueError('Input file doesn\'t exist.')
     if args.output_file:
-        print("Output file: " + args.output_file)
+        print('Output file: ' + args.output_file)
         output_file_splitted = args.output_file.split('.')
         if len(output_file_splitted) != 1 and output_file_splitted[1] != 'png' or len(output_file_splitted) > 2:
-            raise ValueError("Invalid outputfile name.")
+            raise ValueError('Invalid output file name.')
+
+
+def parse_json(json_dict):
+    if not {'Figures', 'Screen', 'Palette'}.issubset(set(json_dict.keys())):
+        raise SyntaxError('Invalid JSON syntax.')
+    figures = []
+    for fig in json_dict['Figures']:
+        fig_type = fig['type']
+        print(fig)
+        if fig_type == 'point':
+            if 'x' not in fig or 'y' not in fig:
+                raise SyntaxError('Invalid JSON syntax - point.')
+        elif fig_type == 'polygon':
+            if 'points' not in fig:
+                raise SyntaxError('Invalid JSON syntax - polygon.')
+        elif fig_type == 'rectangle':
+            if not {'x', 'y', 'height', 'width'}.issubset(set(fig.keys())):
+                raise SyntaxError('Invalid JSON syntax - rectangle.')
+        elif fig_type == 'square':
+            if not {'x', 'y', 'size'}.issubset(set(fig.keys())):
+                raise SyntaxError('Invalid JSON syntax - square.')
+        elif fig_type == 'circle':
+            if not {'x', 'y', 'radius'}.issubset(set(fig.keys())):
+                raise SyntaxError('Invalid JSON syntax - circle.')
 
 
 def main():
     parser = ArgumentParser()
     parser.add_argument("input_file", help="input file name")
-    parser.add_argument("-o", "--output_file", nargs="?", help = "output file name (optional)")
+    parser.add_argument("-o", "--output_file", nargs="?", help="output file name (optional)")
 
     args = parser.parse_args()
     try:
@@ -28,24 +57,25 @@ def main():
         exit(1)
 
     with open(args.input_file, 'r') as f:
-        d = json.load(f)
-        print(d)
-        # print(d["Figures"])
-        for fig in d["Figures"]:
-            print(fig['type'])
-        print(d["Palette"])
+        json_dict = json.load(f)
 
-    win = GraphWin('JSON picture', 650, 400)
-    pt = Point(100, 50)
-    pt.draw(win)
+    print()
 
-    win.postscript(file="image.eps", colormode='color')
-    from PIL import Image as NewImage
-    img = NewImage.open("image.eps")
-    img.save(args.output_file, "png")
+    # print(json_dict["Palette"])
+    print(json_dict["Screen"]["width"])
+    print(json_dict["Screen"]["height"])
 
-    win.getMouse()
-    win.close()
+    try:
+        parse_json(json_dict)
+    except Exception as e:
+        print("Error: " + str(e))
+        exit(1)
+
+    figure = plt.figure()
+
+    if args.output_file:
+        figure.savefig('rect1.png', dpi=1)
+
 
 if __name__ == "__main__":
     main()
