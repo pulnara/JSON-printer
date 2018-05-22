63db5fbce42884e759e74102642fb504ea85efa1
pulnara <magnes.agnieszka@gmail.com>
2018-05-22 21:06:51 +0200

    Reorganize code, add decorators

--- figures.py
+++ figures.py
@@ -3,55 +3,104 @@ import matplotlib.pyplot as plt
 
 class Screen:
     def __init__(self, fg_color, bg_color, height, width):
-        self.fg_color = fg_color
-        self.bg_color = bg_color
-        self.height = height
-        self.width = width
+        self.__fg_color = fg_color
+        self.__bg_color = bg_color
+        self.__height = height
+        self.__width = width
+
+    @property
+    def background(self):
+        return plt.Rectangle((0, 0), self.__width, self.__height, color=self.__bg_color)
+
+    @property
+    def canvas(self):
+        return plt.figure(figsize=(self.__width, self.__height), dpi=1)
+
+    @property
+    def fg_color(self):
+        return self.__fg_color
 
 
 class Figure:
     def __init__(self, color):
-        self.artist = None
-        self.color = color
+        self._color = color
 
-    def get_artist(self):
-        return self.artist
+    def artist(self):
+        return None
+
+    @staticmethod
+    def create(color, fig):
+        return None
 
 
 class Point(Figure):
     def __init__(self, color, x, y):
         super().__init__(color)
-        self.x = x
-        self.y = y
-        self.artist = plt.Circle((self.x, self.y), 1, color=self.color)
+        self.__x = x
+        self.__y = y
+
+    def artist(self):
+        return plt.Circle((self.__x, self.__y), 1, color=self._color)
+
+    @staticmethod
+    def create(color, fig):
+        return Point(color, fig['x'], fig['y'])
 
 
 class Polygon(Figure):
     def __init__(self, color, points):
         super().__init__(color)
-        self.points = points
-        self.artist = plt.Polygon(self.points, color=self.color)
+        self.__points = points
+
+    def artist(self):
+        return plt.Polygon(self.__points, color=self._color)
+
+    @staticmethod
+    def create(color, fig):
+        return Polygon(color, fig['points'])
 
 
 class Rectangle(Figure):
     def __init__(self, color, x, y, height, width):
         super().__init__(color)
-        self.x = x
-        self.y = y
-        self.artist = plt.Rectangle((self.x, self.y), width, height, color=self.color)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    def artist(self):
+        return plt.Rectangle((self.__x, self.__y), self.__width, self.__height, color=self._color)
+
+    @staticmethod
+    def create(color, fig):
+        return Rectangle(color, fig['x'], fig['y'], fig['height'], fig['width'])
 
 
 class Square(Figure):
     def __init__(self, color, x, y, size):
         super().__init__(color)
-        self.artist = plt.Rectangle((x, y), size, size, color=color)
+        self.__x = x
+        self.__y = y
+        self.__size = size
+
+    def artist(self):
+        return plt.Rectangle((self.__x, self.__y), self.__size, self.__size, color=self._color)
+
+    @staticmethod
+    def create(color, fig):
+        return Square(color, fig['x'], fig['y'], fig['size'])
 
 
 class Circle(Figure):
     def __init__(self, color, x, y, radius):
         super().__init__(color)
-        self.x = x
-        self.y = y
-        self.radius = radius
-        self.artist = plt.Circle((self.x, self.y), radius=self.radius, color=self.color)
+        self.__x = x
+        self.__y = y
+        self.__radius = radius
 
+    def artist(self):
+        return plt.Circle((self.__x, self.__y), radius=self.__radius, color=self._color)
+
+    @staticmethod
+    def create(color, fig):
+        return Circle(color, fig['x'], fig['y'], fig['radius'])
--- json-printer.py
+++ json-printer.py
@@ -5,6 +5,10 @@ from ast import literal_eval
 from figures import *
 
 
+class JSONSyntaxError(Exception):
+    pass
+
+
 def check_args(args):
     print('Input file: ' + args.input_file)
     input_file_splitted = args.input_file.split('.')
@@ -37,18 +41,18 @@ def get_color(palette, color):
     elif color[0] == '#':
         result = color
     else:
-        raise SyntaxError('Invalid JSON syntax - unknown color.')
+        raise JSONSyntaxError('Invalid JSON syntax - unknown color.')
     return result
 
 
 def parse_json(json_dict):
     if not {'Figures', 'Screen', 'Palette'}.issubset(set(json_dict.keys())):
-        raise SyntaxError('Invalid JSON syntax.')
+        raise JSONSyntaxError('Invalid JSON syntax.')
 
     figures = []
 
     if not {'fg_color', 'bg_color', 'height', 'width'}.issubset(set(json_dict['Screen'].keys())):
-        raise SyntaxError("Invalid JSON syntax - screen.")
+        raise JSONSyntaxError("Invalid JSON syntax - screen.")
 
     screen_tmp = json_dict['Screen']
     screen = Screen(screen_tmp['fg_color'], screen_tmp['bg_color'], screen_tmp['height'], screen_tmp['width'])
@@ -64,34 +68,34 @@ def parse_json(json_dict):
 
         if fig_type == 'point':
             if 'x' not in fig or 'y' not in fig:
-                raise SyntaxError('Invalid JSON syntax - point.')
-            figures.append(Point(color, fig['x'], fig['y']))
+                raise JSONSyntaxError('Invalid JSON syntax - point.')
+            figures.append(Point.create(color, fig))
         elif fig_type == 'polygon':
             if 'points' not in fig:
-                raise SyntaxError('Invalid JSON syntax - polygon.')
-            figures.append(Polygon(color, fig['points']))
+                raise JSONSyntaxError('Invalid JSON syntax - polygon.')
+            figures.append(Polygon.create(color, fig))
         elif fig_type == 'rectangle':
             if not {'x', 'y', 'height', 'width'}.issubset(set(fig.keys())):
-                raise SyntaxError('Invalid JSON syntax - rectangle.')
-            figures.append(Rectangle(color, fig['x'], fig['y'], fig['height'], fig['width']))
+                raise JSONSyntaxError('Invalid JSON syntax - rectangle.')
+            figures.append(Rectangle.create(color, fig))
         elif fig_type == 'square':
             if not {'x', 'y', 'size'}.issubset(set(fig.keys())):
-                raise SyntaxError('Invalid JSON syntax - square.')
-            figures.append(Square(color, fig['x'], fig['y'], fig['size']))
+                raise JSONSyntaxError('Invalid JSON syntax - square.')
+            figures.append(Square.create(color, fig))
         elif fig_type == 'circle':
             if not {'x', 'y', 'radius'}.issubset(set(fig.keys())):
-                raise SyntaxError('Invalid JSON syntax - circle.')
-            figures.append(Circle(color, fig['x'], fig['y'], fig['radius']))
+                raise JSONSyntaxError('Invalid JSON syntax - circle.')
+            figures.append(Circle.create(color, fig))
 
     return screen, figures
 
 
 def draw(args, figures, screen):
-    picture = plt.figure(figsize=(screen.width, screen.height), dpi=1)
-    picture.artists.append(plt.Rectangle((0, 0), screen.width, screen.height, color=screen.bg_color))
+    picture = screen.canvas
+    picture.artists.append(screen.background)
 
     for fig in figures:
-        picture.artists.append(fig.get_artist())
+        picture.artists.append(fig.artist())
 
     plt.show()
     if args.output_file:
